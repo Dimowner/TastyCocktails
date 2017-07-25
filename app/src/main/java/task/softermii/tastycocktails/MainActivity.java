1d0d391f89ffea09bfc47900db8d117155688243
Dimowner <Dimmony@gmail.com>
2017-07-25 23:45:22 +0300

    Added app icon, branded lunch, app colors

--- app/src/main/java/task/softermii/tastycocktails/MainActivity.java
+++ app/src/main/java/task/softermii/tastycocktails/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.AppTheme);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
