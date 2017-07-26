a225f97f115baf15618b4b9de422d41a062e980e
Dimowner <Dimmony@gmail.com>
2017-07-26 15:41:58 +0300

    Added login with Facebook

--- app/src/main/java/task/softermii/tastycocktails/login/LoginActivity.java
+++ app/src/main/java/task/softermii/tastycocktails/login/LoginActivity.java
@@ -1,72 +1,84 @@
-/*
- * Copyright 2017 Dmitriy Ponomarenko
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package task.softermii.tastycocktails;
+package task.softermii.tastycocktails.login;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+import butterknife.ButterKnife;
+import task.softermii.tastycocktails.R;
+import task.softermii.tastycocktails.cocktails.CocktailsActivity;
+import task.softermii.tastycocktails.util.UIUtil;
 import timber.log.Timber;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created on 26.07.2017.
+ * @author Dimowner
+ */
+public class LoginActivity extends AppCompatActivity {
 
 	CallbackManager callbackManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		setTheme(R.style.AppTheme);
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_login);
+		ButterKnife.bind(this);
 
 		callbackManager = CallbackManager.Factory.create();
 		LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+		loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday"));
 		loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
 			@Override
 			public void onSuccess(LoginResult loginResult) {
-				AccessToken token = AccessToken.getCurrentAccessToken();
-				Timber.v("OnSuccess userID = " + token.getUserId() + " token = " + token.getToken());
+				startCocktailsActivity();
+
 			}
 
 			@Override
 			public void onCancel() {
-				Timber.v("onCancel");
 			}
 
 			@Override
 			public void onError(FacebookException e) {
 				Timber.e(e);
+				UIUtil.showWarningDialog(LoginActivity.this, R.string.msg_error_on_login);
 			}
 		});
+
+		if (isLogged()) {
+			startCocktailsActivity();
+		}
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		callbackManager.onActivityResult(requestCode, resultCode, data);
+	}
+
+	public void startCocktailsActivity() {
+		Intent intent = new Intent(getApplicationContext(), CocktailsActivity.class);
+		startActivity(intent);
+	}
+
+	private boolean isLogged() {
+		return Profile.getCurrentProfile() != null && AccessToken.getCurrentAccessToken() != null;
 	}
 }
